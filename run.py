80376738fa75ef06a3b5b1aa1b840a26428d0a7d
saramorrin <sara.lee.morrin@googlemail.com>
2021-11-09 17:34:34 +0000

    Create function to get user guess inpput and run checks to ensure that data is valid

--- run.py
+++ run.py
@@ -3,15 +3,18 @@ import random
 def welcome_message():
     """
     Print statements used in the welcome message,
-    will display each time a new game begins
+    will display each time a new game begins.
     """
-  
     print("Welcome to Battleships")
     print("You have 5 pirate ships to sink")
     player_name = input("Please enter your name Captain: ")
 
 
 class GameBoard:
+    """
+    Create computer gameboard using letters for x row 
+    and numbers for y row.
+    """
     def __init__(self, board):
         self.board = board
 
@@ -29,6 +32,11 @@ class GameBoard:
 
 
 class Battleship:
+    """
+    Create 5 ships on computer gameboard in randomly 
+    generated locations. These ships will be hidden 
+    from the player.
+    """
     def __init__(self, board):
         self.board = board
 
@@ -39,6 +47,39 @@ class Battleship:
                 self.x_row, self.y_column = random.randint(0, 3), random.randint(0, 3)
             self.board[self.x_row][self.y_column] = "x"
         return self.board
+
+
+    def get_user_input(self):
+        """
+        Requests player input of co-ordinates to guess location of a
+        computers ship. Allows for while loop to run to ensure that the 
+        data inputted is valid, error message will display to user if 
+        data is not valid. Loop will repeatedly request data until it 
+        is valid.
+        """
+    try:
+        x_row = input("Enter the row of the ship between 1-4: ")
+        while x_row not in '1234':
+            print('Uh oh, you must enter a value between 1-4')
+            x_row = input("Enter the row of the ship between 1-4: ")
+
+        y_column = input("Enter the column letter of the ship between A-D: ").upper()
+        while y_column not in "ABCD":
+            print('Uh oh, you must enter a letter between A-D')
+            y_column = input("Enter the column letter of the ship between A-D: ").upper()
+            return int(x_row) - 1, GameBoard.get_letters_to_numbers()[y_column]
+        #except ValueError and KeyError:
+            #print("Uh oh, that was not a valid input")
+            return self.get_user_input()
+
+
+    def count_hit_ships(self):
+        hit_ships = 0
+        for row in self.board:
+            for column in row:
+                if column == "x":
+                    hit_ships += 1
+    return hit_ships
 
 
 def new_game():
