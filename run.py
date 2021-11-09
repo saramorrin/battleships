c58170954eca48dd428bfb7a9fa37c46ca8983e9
saramorrin <sara.lee.morrin@googlemail.com>
2021-11-09 16:53:59 +0000

    Create welcome message function to be displayed when new game is run

--- run.py
+++ run.py
@@ -1,1 +1,19 @@
-from random import randint
+import random
+
+def welcome_message():
+    """
+    Print statements used in the welcome message,
+    will display each time a new game begins
+    """
+  
+    print("Welcome to Battleships")
+    print("You have 5 pirate ships to sink")
+    player_name = input("Please enter your name Captain: ")
+
+
+def new_game():
+
+    welcome_message()
+  
+
+new_game()
